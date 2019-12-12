504a8cae752a210bc0449f0a29aa79d1bee7df1d
Nikhil Kumar <47697153+importnikhil@users.noreply.github.com>
2019-12-12 19:44:24 +0530

    Update ATM.cpp

--- ATM.cpp
+++ ATM.cpp
@@ -1,6 +1,6 @@
 /* ***QUESTION***
 ***SOURCE:CODECHEF*** 
-***AVAILABLE IN -> Name- ATM -> Code - HS08TEST : PRACTICE(EASY)***
+***AVAILABLE IN -> Name- ATM -> Code - HS08TEST : PRACTICE(Beginner)***
 
 Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction if X is a multiple of 5, and Pooja's account balance has enough cash to perform the withdrawal transaction (including bank charges). For each successful withdrawal the bank charges 0.50 $US. Calculate Pooja's account balance after an attempted transaction.
 
